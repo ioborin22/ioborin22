7154092ac92e097d97c55da913e8a68aeb78848a
ioborin22 <ioborin22@gmail.com>
2023-06-29 19:22:42 -0400

    20230629

--- app/Http/Controllers/API/MessageController.php
+++ app/Http/Controllers/API/MessageController.php
@@ -41,26 +41,37 @@ class MessageController extends Controller
     }
 
     /**
-     * Display massage.
+     * Display messages.
      */
     public function show($senderId, $receiverId, Request $request)
     {
-        $messages = Message::where('sender_id', $senderId)
-            ->where('receiver_id', $receiverId);
+        // Query for messages where sender_id is $senderId and receiver_id is $receiverId
+        // or where sender_id is $receiverId and receiver_id is $senderId
+        $messages = Message::where(function ($query) use ($senderId, $receiverId) {
+            $query->where('sender_id', $senderId)
+                ->where('receiver_id', $receiverId);
+        })
+            ->orWhere(function ($query) use ($senderId, $receiverId) {
+                $query->where('sender_id', $receiverId)
+                    ->where('receiver_id', $senderId);
+            });
 
+        // Check if the request has a 'limit' parameter
         if ($request->has('limit')) {
             $limit = $request->query('limit');
+            // Paginate the messages based on the provided limit
             $messages = $messages->paginate($limit);
         } else {
+            // Retrieve all messages
             $messages = $messages->get();
         }
 
+        // Check if the messages collection is empty
         if ($messages->isEmpty()) {
             return response()->json(['message' => 'Message not found'], 404);
         }
 
+        // Return the messages as a JSON response
         return response()->json($messages);
     }
-
-
 }
